d1ec6370184e57c41de9c517d391bb01179aa2c4
Dan Schult <dschult@colgate.edu>
2017-09-09 22:22:05 -0400

    Add htaccess and robots files

--- doc/conf.py
+++ doc/conf.py
@@ -8,3 +8,16 @@ source_parsers = {
 source_suffix = ['.md']
 master_doc = 'index'
 
+# -- Hack to copy index.html file to output
+from shutil import copyfile
+
+def copy_html(app, docname):
+    if app.builder.name == "html":
+        for filename in ["/.htaccess", "/robots.txt"]:
+            print("Copying ",filename," to ", app.outdir)
+            source = app.srcdir + filename
+            target = app.outdir + filename
+            copyfile(source, target)
+
+def setup(app):
+    app.connect('build-finished', copy_html)
